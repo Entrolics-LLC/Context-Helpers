9de7cf1518e73e396b93661142208df76b8b528a
mansoor876 <mansoor.hussain@mach3bi.com>
2022-06-15 13:27:59 +0500

    updated run query

--- helpers.js
+++ helpers.js
@@ -1,15 +1,8 @@
 
 const runQuery = async (db, query) => {
-    try {
-        let queryType = query?.split(' ')[0]?.toUpperCase()
-        const data = await db.query(query, { type: db?.QueryTypes?.[queryType] })
-        return Array.isArray(data) ? data?.flat() : data
-    }
-    catch (e) {
-        console.log('e', e)
-        return []
-    }
-
+    let queryType = query?.split(' ')[0]?.toUpperCase()
+    const data = await db.query(query, { type: db?.QueryTypes?.[queryType] })
+    return Array.isArray(data) ? data?.flat() : data
 }
 
 module.exports = {
