8736988c8108c774a06e4e8534c37d664763dd74
mansoor876 <mansoor.hussain@mach3bi.com>
2022-06-29 16:45:47 +0500

    feat/ added video_json model

---
+++ migrations/context/20220629114148-create-video-json.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('video_jsons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      time_stamp: {
+        type: Sequelize.DATE
+      },
+      file_name: {
+        type: Sequelize.STRING
+      },
+      video_json: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('video_jsons');
+  }
+};
---
+++ models/context/video_json.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class video_json extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  video_json.init({
+    time_stamp: DataTypes.DATE,
+    file_name: DataTypes.STRING,
+    video_json: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'video_json',
+  });
+  return video_json;
+};
